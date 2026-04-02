27ecc5cf08e6885f68c4cde980984e3964d2b5a3
KP-isme <kpnot2day@gmail.com>
2026-04-02 14:15:42 -0400

    adding player
    player

---
+++ js/Player.js
@@ -0,0 +1,22 @@
+function Player()
+{
+    // setting up player's starting point
+
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+
+    //player size
+    this.width = 100;
+    this.height = 100;
+
+    //this is the color for player
+    this.color = "#ff3232ff";
+    
+
+
+
+
+
+
+
+}
